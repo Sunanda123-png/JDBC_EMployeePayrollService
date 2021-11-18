0d206177179f4a34f1004ad0252df2c1464a2325
Sunanda123-png <sunanda02@gmail.com>
2021-11-18 18:46:15 +0530

    Retrive the data from database

--- src/com/bridgelabz/JdbcPayrollService.java
+++ src/com/bridgelabz/JdbcPayrollService.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 import java.sql.*;
 
 public class JdbcPayrollService {
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) throws Exception{
+
             //load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             //create connection
@@ -12,16 +12,21 @@ public class JdbcPayrollService {
             String password="1234";
             Connection con=DriverManager.getConnection(url,username,password);
 
-            if (con.isClosed()){
-                System.out.println("Connection is closed");
-            }
-            else{
-                System.out.println("Connection is created");
+            String query="select * from employee_payroll";
+            Statement stat= con.createStatement();
+            ResultSet set=stat.executeQuery(query);
+            while(set.next()){
+                int id =set.getInt(1);
+                String name= set.getString(2);
+                String gender= set.getString(3);
+                int salary =set.getInt(4);
+                Date date=set.getDate(5);
+                System.out.println(id+";"+name+";"+gender+";"+salary+";"+date);
+
             }
 
+            stat.close();
+            con.close();
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
