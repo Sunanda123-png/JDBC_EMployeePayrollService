4da69f3dae522a0921930da69ccca3220362ff66
Sunanda123-png <sunanda02@gmail.com>
2021-11-18 22:43:59 +0530

    Add: Get the data in given date range

--- src/com/bridgelabz/JdbcPayrollService.java
+++ src/com/bridgelabz/JdbcPayrollService.java
@@ -1,19 +1,28 @@
 package com.bridgelabz;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 public class JdbcPayrollService {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
             //load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             //create connection
-            String url="jdbc:mysql://localhost:3306/payroll_service1";
-            String username="root";
-            String password="1234";
-            Connection con=DriverManager.getConnection(url,username,password);
-            Statement stat= con.createStatement();
-            String sql="update Employee3 set salary=3200000 where id=5";
-            stat.executeUpdate(sql);
-            System.out.println("Done");
+            String url = "jdbc:mysql://localhost:3306/payroll_service1";
+            String username = "root";
+            String password = "1234";
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement stat = con.createStatement();
+            String query = "SELECT * FROM Employee3 WHERE startJobDate BETWEEN 2021-10-15 AND 2021-11-15";
+            ResultSet set = stat.executeQuery(query);
+            while (set.next()) {
+                    int id = set.getInt(1);
+                    String name = set.getString(2);
+                    String gender = set.getString(3);
+                    int salary = set.getInt(4);
+                    Date date = set.getDate(5);
+                    System.out.println(id + ";" + name + ";" + gender + ";" + salary + ";" + date);
+            }
             stat.close();
             con.close();
 
