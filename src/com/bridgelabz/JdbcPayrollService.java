5746058eea50797441a30d2291b700fee06652a1
Sunanda123-png <sunanda02@gmail.com>
2021-11-18 22:01:07 +0530

    Add: Update the salary of Terisa

--- src/com/bridgelabz/JdbcPayrollService.java
+++ src/com/bridgelabz/JdbcPayrollService.java
@@ -1,30 +1,19 @@
 package com.bridgelabz;
 import java.sql.*;
-
 public class JdbcPayrollService {
     public static void main(String[] args) throws Exception{
 
             //load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             //create connection
-            String url="jdbc:mysql://localhost:3306/payroll_service";
+            String url="jdbc:mysql://localhost:3306/payroll_service1";
             String username="root";
             String password="1234";
             Connection con=DriverManager.getConnection(url,username,password);
-
-            String query="select * from employee_payroll";
             Statement stat= con.createStatement();
-            ResultSet set=stat.executeQuery(query);
-            while(set.next()){
-                int id =set.getInt(1);
-                String name= set.getString(2);
-                String gender= set.getString(3);
-                int salary =set.getInt(4);
-                Date date=set.getDate(5);
-                System.out.println(id+";"+name+";"+gender+";"+salary+";"+date);
-
-            }
-
+            String sql="update Employee3 set salary=3200000 where id=5";
+            stat.executeUpdate(sql);
+            System.out.println("Done");
             stat.close();
             con.close();
 
